891335819ea9b7d0dc36a4a51ced90913354c934
inasrenan <inasrenan@github.com>
2017-10-20 20:31:31 +0700

    pembuatan rumus dan menyelesaikan maslahnya

--- tugas0204.cpp
+++ tugas0204.cpp
@@ -4,4 +4,12 @@ using namespace std;
 int main(){
 	
 	double luasrumah,par=1.87596,jumblah,biayapar,biayapas,jmlhbox,totalbilgn,x,y;
+	
+	cout<<"luas rumah\t\t: ";
+	cin>>luasrumah;
+	cout<<"jumblah box Parket\t\t: ";cin>>jumblah;
+	cout<<"-------------------------------------------------"<<endl;
+	x=par*jumblah;
+	y=luasrumah/x;
+	jmlhbox=y*234.495;
 }
